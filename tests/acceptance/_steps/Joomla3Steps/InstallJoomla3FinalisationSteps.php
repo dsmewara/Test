e7d8a1e27a24b02714dc595ea65ff42d90d15237
Dalpat <dalpatsingh@tasolglobal.com>
2015-04-29 10:54:24 +0530

    update installjoomla3finalstep.php and acceptancesuite.dist.yml

--- tests/acceptance/_steps/Joomla3Steps/InstallJoomla3FinalisationSteps.php
+++ tests/acceptance/_steps/Joomla3Steps/InstallJoomla3FinalisationSteps.php
@@ -38,7 +38,7 @@ class InstallJoomla3FinalisationSteps extends InstallJoomla3Steps
 		}
 
 		$I->click('Install');
-        shell_exec('sudo chmod /var/www/html/Joomla_3.4.1', 0777);
+        shell_exec('sudo chmod /var/www/html/jcms3.4', 0777);
 		$I->waitForElement(\InstallJoomla3ManagerPage::$removeInstallationFolder, 60);
         $I->wait(2);
 	}
